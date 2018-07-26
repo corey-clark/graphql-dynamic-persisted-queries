7bc3cbb63faecb596cad8fdc67485ee9288f02fc
corey.clark <corey.clark@ticketmaster.com>
2018-07-25 23:34:50 -0700

    use ioredis instead of apollo redis

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const { ApolloServer } = require('apollo-server-express')
-const { RedisCache } = require('apollo-server-cache-redis')
+const Redis = require('ioredis')
 const typeDefs = require('./schemas')
 const resolvers = require('./resolvers')
 
 const port = 4000
 const app = express()
+const cache = new Redis()
+const connected = client.status === 'ready'
+const persistedQueries = connected ? { persistedQueries: { cache } } : {}
 
-const server = new ApolloServer({ 
-  typeDefs, 
+const server = new ApolloServer({
+  typeDefs,
   resolvers,
-  persistedQueries: {
-    cache: new RedisCache()
-  }
+  ...persistedQueries,
 })
 
 server.applyMiddleware({ app })
